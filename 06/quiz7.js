85984a8ae88c8e876da495be36b9c83eb79c1da1
KO SE JONG <fulfilled_@naver.com>
2022-04-14 16:04:24 +0900

    quiz7

---
+++ 06/quiz7.js
@@ -0,0 +1,41 @@
+const point = document.querySelector('#point');
+const life = document.querySelector('#life');
+const box = document.querySelector('.box');
+const bug = document.querySelector('#bug');
+
+
+function toggleBug() {
+    if (bug.style.display == 'block' || bug.style.display == ''){
+        bug.style.display = 'none';
+    }
+    else if(bug.style.display == 'none'){
+        bug.style.display = 'block';
+        randPosition(box.clientWidth, bug.clientWidth);
+    }
+};
+
+function randPosition(boxWidth, bugWidth) {
+    bug.style.left = Math.floor(Math.random() * (boxWidth - bugWidth)) + 1 + 'px';
+    bug.style.top = Math.floor(Math.random() * (boxWidth - bugWidth)) + 1 + 'px';
+}
+
+
+window.addEventListener('load', function() {
+    setInterval(toggleBug, 2000);
+});
+
+
+box.addEventListener('click', function(event) {
+    if (event.target == bug) {
+        point.innerHTML = parseInt(point.innerHTML) + 1;
+        bug.style.display = 'none';
+    }
+    else if (event.target == box) {
+        life.innerHTML = parseInt(life.innerHTML) - 1;
+    }
+
+    if (life.innerHTML == 0) {
+        alert('gameover');
+        location.reload();
+    }
+})
