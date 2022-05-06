870cbcd98d74f49b987769122aab065b6caf765e
SebellKo <fulfilled_@naver.com>
2022-05-07 02:54:15 +0900

    randNum

--- 07/quiz8.js
+++ 07/quiz8.js
@@ -16,14 +16,36 @@ maxNum.addEventListener('change', (event) => {
     maxInputNum = event.target.value;
 })
 
-btn.addEventListener('click', () => {
-    box.innerHTML = createRandom(minInputNum, maxInputNum);
-    console.log(minInputNum, maxInputNum);
-});
+// btn.addEventListener('click', () => {
+//     box.innerHTML = createRandom(minInputNum, maxInputNum);
+//     console.log(box);
+// });
 
 function createRandom(min, max) {
-   return Math.floor(Math.random() * (max - min + 1)) + min
+   return Math.floor((Math.random() * (max - min + 1)) + min)
 }
+
+// function countNum(randNum, inputNum) {
+//     setInterval(() => {
+//         if (inputNum === randNum) {
+//             return;
+//         }
+//         inputNum++;
+//     }, 20);
+// }
+
+btn.addEventListener('click', () => {
+    box.innerHTML = 0;
+    let randNum = parseInt(createRandom(minInputNum, maxInputNum));
+    const interval = setInterval(function() {
+        if (box.innerHTML == randNum) {
+            clearInterval(interval);
+            box.innerHTML = randNum;
+        }
+        box.innerHTML++;
+        console.log('0')
+    }, 100);
+})
 
 
 
